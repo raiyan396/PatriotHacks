773fa065e5ae3fab2e5b489ee0b21bd9991d1a69
Amro Karar <amrokarar@gmail.com>
2023-10-08 10:06:33 -0400

    api addition

--- aipdfreader/pdfreader.py
+++ aipdfreader/pdfreader.py
@@ -1,30 +1,109 @@
 import fitz
 import openai
+from flask import Flask, request
 
-doc = fitz.open('aipdfreader/schedule.pdf')
+app = Flask(__name__)
 
-for page in doc:
-    text = page.get_text()
-    
-# Set your OpenAI API key
-openai.api_key = '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'
 
-# Define the conversation in the format for chat completions
-conversation = [
-    {"role": "user", "content": "build an .ics calendar file based on the assignments and exams in this file: " + text},
-]
+@app.route('/')
+def home():
+    return 'This is the home page'
 
-# Make a request to the GPT-3.5-turbo model for completions
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=conversation,
-    temperature=0.8,
-    max_tokens=2000
-)
 
-file = open('read.ics', 'w') 
-file.write(response.choices[0].message['content']) 
-print("Printed Successfully")
-file.close()
-# Print the response
+@app.route('/upload-pdf', methods=['POST'])
+def upload_pdf():
+    global pdf_text
 
+    if 'pdfFile' not in request.files:
+        return 'No file part'
+
+    pdf_file = request.files['pdfFile']
+
+    if pdf_file.filename == '':
+        return 'No selected file'
+
+    pdf_contents = pdf_file.read()
+
+    # Create a PDF document object from the binary content
+    pdf_document = fitz.open(stream=pdf_contents, filetype="pdf")
+
+    # Initialize an empty string to store the extracted text
+    text = ""
+
+    # Iterate through the pages and extract text
+    for page in pdf_document:
+        text += page.get_text()
+
+    pdf_text = text
+    # Print the extracted text
+    print(pdf_text)
+
+    print('We got this far\n')
+    # Now you can use pdf_text to generate the .ics file
+    if pdf_text:
+        print('In the if statement\n')
+        # Set your OpenAI API key
+        openai.api_key = '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'
+
+        # Define the conversation in the format for chat completions
+        conversation = [
+            {"role": "user", "content": "build an .ics calendar file based on the assignments and exams in this file: " + pdf_text},
+        ]
+
+        print("Conversed\n")
+        # Make a request to the GPT-3.5-turbo model for completions
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=conversation,
+            temperature=0.8,
+            max_tokens=2000
+        )
+        print(response)
+        print("were starting to write\n")
+        # Write the response to the .ics file
+        with open('read.ics', 'w') as file:
+            file.write(response.choices[0].message['content'])
+
+        print("Printed Successfully")
+        return 'ICS file generated successfully'
+    else:
+        return 'PDF text not available, please upload a PDF file first'
+
+    return 'PDF file uploaded successfully'
+
+
+# @app.route('/generate-ics', methods=['POST'])
+# def generate_ics():
+#     global pdf_text
+#     print('We got this far\n')
+#     # Now you can use pdf_text to generate the .ics file
+#     if pdf_text:
+#         print('In the if statement\n')
+#         # Set your OpenAI API key
+#         openai.api_key = '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'
+
+#         # Define the conversation in the format for chat completions
+#         conversation = [
+#             {"role": "user", "content": "build an .ics calendar file based on the assignments and exams in this file: " + pdf_text},
+#         ]
+
+#         # Make a request to the GPT-3.5-turbo model for completions
+#         response = openai.ChatCompletion.create(
+#             model="gpt-3.5-turbo",
+#             messages=conversation,
+#             temperature=0.8,
+#             max_tokens=2000
+#         )
+
+#         # Write the response to the .ics file
+#         with open('read.ics', 'w') as file:
+#             file.write(response.choices[0].message['content'])
+
+#         print("Printed Successfully")
+#         return 'ICS file generated successfully'
+#     else:
+#         return 'PDF text not available, please upload a PDF file first'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
--- patty/src/components/PDFGrab.js
+++ patty/src/components/PDFGrab.js
@@ -14,10 +14,16 @@ function PDFGrab() {
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
   const [pdfData, setPdfData] = useState(null);
+  const [file, setFile] = useState(null);
 
   // Event handler for file load
-  const onFileLoad = ({ target: { result } }) => {
-    setPdfData(result);
+  //   const onFileLoad = ({ target: { result } }) => {
+  //     setPdfData(result);
+  //   };
+
+  const handleFileChange = (event) => {
+    const selectedFile = event.target.files[0];
+    setFile(selectedFile);
   };
 
   // Event handler for successful document load
@@ -25,13 +31,49 @@ function PDFGrab() {
     setNumPages(numPages);
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("pdfFile", file);
+
+    try {
+      const response = await fetch("http://127.0.0.1:8000/upload-pdf", {
+        method: "POST",
+        body: formData,
+      });
+
+      // Check if the upload-pdf request was successful
+      if (response.ok) {
+        // If successful, proceed to send the generate-ics POST request
+        const response2 = await fetch("http://127.0.0.1:8000/generate-ics", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (response2.ok) {
+          // Handle the success response from the API
+        } else {
+          // Handle errors for the generate-ics request
+        }
+      } else {
+        // Handle errors for the upload-pdf request
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  };
+
   //   const { pageNumber, numPages, pdfData } = this.state;
   return (
-    <>
+    <form onSubmit={handleSubmit}>
       <input
         type="file"
         accept=".pdf"
-        onChange={(event) => this.onFileLoad(event)}
+        // onChange={(event) => this.onFileLoad(event)}
+        onChange={handleFileChange}
       />
 
       {pdfData && (
@@ -45,7 +87,8 @@ function PDFGrab() {
           Page {pageNumber} of {numPages}
         </p>
       )}
-    </>
+      <button type="submit">Submit</button>
+    </form>
   );
 }
 
